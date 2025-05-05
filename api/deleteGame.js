6b9571c565bfb583db9ed57f51ac635d240d9159
Deadpeel <cosminstefan2308@gmail.com>
2025-05-06 00:12:47 +0300

    fixed delete

--- api/deleteGame.js
+++ api/deleteGame.js
@@ -1,19 +1,17 @@
-import { MongoClient } from "mongodb";
-
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
-
 export default async function handler(req, res) {
+  const { id } = req.query;
   try {
-    await client.connect();
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
     const db = client.db("cloud-computing-project");
     const collection = db.collection("games");
 
-    const data = await collection.deleteOne({ _id: req.body });
-    res.status(200).json(data);
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: "Jocul nu a fost gasit." });
+    }
+
+    res.status(200).json({ message: "Jocul a fost sters cu succes." });
   } catch (error) {
     res.status(500).json({ error: error.message });
-  } finally {
-    await client.close();
   }
 }
