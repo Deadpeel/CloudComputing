039f0d0f1f24f9627bef51a9109ca1ffa8ff52df
Deadpeel <cosminstefan2308@gmail.com>
2025-05-06 00:26:44 +0300

    fixed delete 4

--- api/deleteGame.js
+++ api/deleteGame.js
@@ -10,7 +10,9 @@ export default async function handler(req, res) {
     const db = client.db("cloud-computing-project");
     const collection = db.collection("games");
 
-    const result = await collection.deleteOne({ _id: id });
+    const objectId = new ObjectId(id);
+
+    const result = await collection.deleteOne({ _id: objectId });
     if (result.deletedCount === 0) {
       return res.status(404).json({ error: "Jocul nu a fost gasit." });
     }
